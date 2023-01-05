d0b4ce6e93cfe26163958b3089abafc1ee13d6cf
crkn07 <113365562+crkn07@users.noreply.github.com>
2023-01-05 16:50:55 +0100

    added GNL

---
+++ Libft/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyacoub- <cyacoub-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 18:55:02 by cyacoub-          #+#    #+#             */
+/*   Updated: 2022/09/20 16:10:49 by cyacoub-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+//#include <stdio.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*int main(void)
+{
+	char    str[] = "Hola mundo";
+	printf("la longitud es = %ld\n", ft_strlen(str));
+}*/
